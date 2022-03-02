6231e34d1c3dd1aa23e78f6aea0c0f763fe494f3
W3fring <100790003+W3fring@users.noreply.github.com>
2022-03-02 17:43:50 +0100

    Add files via upload

---
+++ esv2.py
@@ -0,0 +1,55 @@
+import smtplib
+import ssl
+from email.message import EmailMessage
+from time import sleep
+mailboks = """"
+    _________
+   |\       /|
+   | \     / |
+   |  `...'  |
+   |__/___\_
+"""
+#varibler
+count = 0 # en telle variabler for hvor mange som er sendt
+print(mailboks + 'tool by Wefring')
+Subject = input('mail topic: ') 
+body = input('message in the mail: ')
+sender_email = input('bot email: ')  #
+password = input('bot password: ')   #navn og uno two tre
+reciver_mail = input('target: ')
+print('Your bot account', sender_email, ' ', ('with password'), ' ', password, ' ', ('target'), ' >', reciver_mail,)
+print('MAIL PREVIEW:V')
+print(Subject)
+print(body)
+print('MAIL PREVIEW^')
+message = EmailMessage()
+message["From"] = sender_email 
+message["To"] = reciver_mail
+message["Subject"] = Subject 
+message.set_content(body)
+
+def sendmailen(): #
+    context = ssl.create_default_context()
+    print("sending mail NR",count)
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, reciver_mail, message.as_string())
+        print("sent! mail > NR",count) 
+ # for og være sikker på at de har skrevet riktig 
+print('to make sure you entered everything right!')
+conf = input('type confirm to start mail spam > ')
+if conf == "confirm": #når de skriver confirm >
+    print('Starting spam ')
+    for repeat in range(90): #loops 90)
+        count = count+1
+        sendmailen()
+        sleep(0.3)
+else:
+    print('ERROR', ' > ', 'remember to type confirm lowercase!')
+    print('relaunch program and be carefull with spelling ')
+    print('report issues to Wefring')
+    sleep(10)
+    quit()
+print('spam is done total of ', count, 'mails')
+sleep(10)
+quit()
